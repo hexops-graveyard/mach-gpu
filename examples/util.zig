26d2da22b5d3b38aef755689cb7510fd99293a9e
BlueTRaven <taylor@adoniram.net>
2023-11-23 11:12:38 -0500

    migrate to 'local variable is never mutated'

--- examples/util.zig
+++ examples/util.zig
@@ -194,8 +194,7 @@ pub fn msgSend(obj: anytype, sel_name: [:0]const u8, args: anytype, comptime Ret
         else => @compileError("Unsupported number of args"),
     };
 
-    // NOTE: func is a var because making it const causes a compile error which I believe is a compiler bug
-    var func = @as(FnType, @ptrCast(&objc.objc_msgSend));
+    const func = @as(FnType, @ptrCast(&objc.objc_msgSend));
     const sel = objc.sel_getUid(@as([*c]const u8, @ptrCast(sel_name)));
 
     return @call(.auto, func, .{ obj, sel } ++ args);
